dd7133327b10946edaba3125cba994f5cc76b468
Todd Volkert <tvolkert@google.com>
2017-01-23 11:29:01 -0800

    Add AUTHORS file, and add license headers to .sh files

--- dev/travis.sh
+++ dev/travis.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
 
 # Make sure dartfmt is run on everything
 echo "Checking dartfmt..."
